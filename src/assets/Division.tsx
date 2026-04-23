d0be48e5ba75b7238be31cc8c4c156395e662df7
stephaniebrandao <salbuquerque.md@gmail.com>
2026-04-24 00:54:23 +0100

    Add Clipboard and Division components with SVG icons

---
+++ src/assets/Division.tsx
@@ -0,0 +1,20 @@
+function Division({color}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke={color}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-tally1-icon lucide-tally-1"
+    >
+      <path d="M4 4v16" />
+    </svg>
+  );
+}
+
+export default Division;
