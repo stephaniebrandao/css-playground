cbc6a22cd7c6b6b061b70c1139a2f13fcceb0884
stephaniebrandao <salbuquerque.md@gmail.com>
2026-04-24 00:54:05 +0100

    Add ColorPickBackground and ColorPickText components and associated styles

---
+++ src/components/ColorPickBackground.tsx
@@ -0,0 +1,16 @@
+import styles from "./colorPick.module.css"
+
+
+function ColorPickBackground ({label, value, onChange}) {
+    
+    return (
+        <div className={styles.colorPickContainer}>
+            <label>{label}: {value}</label>
+            <input type="color"
+            value={value}
+            onChange={(e) => onChange(String(e.target.value))} />
+        </div>
+    )
+}
+
+export default ColorPickBackground;
